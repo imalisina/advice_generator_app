0e9977a99687bee5d274c514940a7862bad3fa10
Alisina Barghandan <barghandanalisina@gmail.com>
2023-02-15 22:19:30 +0530

    Working on basic file structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
 import '../styles/globals.css';
 
-// Layouts
-import NavbarContainer from '../components/navbar/navbarContainer';
-
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      {/* App navbar layout */}
-      <NavbarContainer />
-
-      {/* Next App */}
       <Component {...pageProps} />
     </>
   )
